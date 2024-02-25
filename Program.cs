f4187a44306eacdf06b09297daf0bc92de14f7af
= <klas_hermansson@hotmail.com>
2024-02-25 19:14:37 +0100

    a start

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿string[] input = File.ReadAllLines("input.txt");
 Dictionary<double, double> seeds = new Dictionary<double, double>();
 double lowestValue = 0;
-KeyValuePair.Key seed =  new();
+KeyValuePair<double, double>seed =  new();
 
 string[] seedArray = input[0].Split(' ');
 
@@ -14,16 +14,27 @@ foreach(KeyValuePair<double, double> kvp in seeds)
 {
     for(double i = kvp.Key; i <= kvp.Key + kvp.Value; i = i + Math.Floor(Math.Sqrt(kvp.Key)))
     {
-        Console.WriteLine(i);
-        if(lowestValue != 0)
+        if(lowestValue == 0)
         {
             lowestValue = i;
-            seed = kvp.Key;
+            seed = kvp;
         }
         if(lowestValue > i)
         {
             lowestValue = i;
-            seed = kvp.Key;
+            seed = kvp;
+        }
+    }
+}
+
+double SeedToLocation(double seed)
+{
+    double internalSeed = seed;
+    foreach (string line in input)
+    {
+        if (line[1].ToString() == "map:")
+        {
+
         }
     }
 }
