20dbb523b854260a76348cd3792db3b03bd4d99a
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2024-02-25 20:40:22 +0100

    Somewhere

--- Program.cs
+++ Program.cs
@@ -12,29 +12,53 @@ for (int i = 1; i < seedArray.Length; i = i + 2)
 
 foreach(KeyValuePair<double, double> kvp in seeds)
 {
-    for(double i = kvp.Key; i <= kvp.Key + kvp.Value; i = i + Math.Floor(Math.Sqrt(kvp.Key)))
+    for(double i = kvp.Key; i <= (kvp.Key + kvp.Value); i++)
     {
         if(lowestValue == 0)
         {
-            lowestValue = i;
+            lowestValue = SeedToLocation(i);
             seed = kvp;
         }
         if(lowestValue > i)
         {
-            lowestValue = i;
+            lowestValue = SeedToLocation(i);
             seed = kvp;
         }
     }
 }
+Console.Write($"lowestValue: {lowestValue}, seed: {seed.Key} - {seed.Key + seed.Value}");
 
-double SeedToLocation(double seed)
+double SeedToLocation(double seedValue)
 {
-    double internalSeed = seed;
+    double source = seedValue;
+    bool getMap = false;
     foreach (string line in input)
     {
-        if (line[1].ToString() == "map:")
+        if (line == string.Empty)
         {
+            getMap = false;
+        }
+        else if(getMap)
+        {
+            string[] map = line.Split(" ");
+            double location = 0;
+            double mapStart = double.Parse(map[0]);
+            double startSource = double.Parse(map[1]);
+            double mapLength = double.Parse(map[2]);
 
+            if(source >= startSource && source <= startSource + mapLength - 1)
+            {
+                location = mapStart + source - startSource;
+                source = location;
+                Console.WriteLine(source);
+            }
+        }
+        else if (line.Contains("map"))
+        {
+            Console.WriteLine(line);
+    Console.WriteLine($"new source: {source} old source: {seedValue}");
+            getMap = true;
         }
     }
+    return source;
 }
