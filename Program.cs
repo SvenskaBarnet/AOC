ca9cb47f2ebe7cc53aaacb41a57d4b4d7b73328e
= <klas_hermansson@hotmail.com>
2024-02-27 09:02:20 +0100

    Start

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] input = File.ReadAllLines("input-test.txt");
+
+string [] times = input[0].Split(' ');
+string [] distances = input[1].Split(' ');
+foreach (string index in input)
+
+
+foreach (string line in times){
+    Console.Write(line + ", ");
+}
+Console.WriteLine();
+foreach (string line in distances){
+    Console.Write(line + ", ");
+}
