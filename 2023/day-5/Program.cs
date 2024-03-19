0737db51845a0f71684824238b1c9fb64b42a103
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2024-03-19 21:43:11 +0100

    Fixed indentation

--- 2023/day-5/Program.cs
+++ 2023/day-5/Program.cs
@@ -3,28 +3,28 @@ Dictionary<double, double> seeds = new Dictionary<double, double>();
 double lowestValue = 0;
 bool lowestValueAssigned = false;
 lowestValueAssigned = false;
-KeyValuePair<double, double>seed =  new();
+KeyValuePair<double, double> seed = new();
 double lowestSeed = 0;
 
 string[] seedArray = input[0].Split(' ');
 
 for (int i = 1; i < seedArray.Length; i = i + 2)
 {
-    seeds.Add(double.Parse(seedArray[i]), double.Parse(seedArray[i+1]));    
+    seeds.Add(double.Parse(seedArray[i]), double.Parse(seedArray[i + 1]));
 }
 
-foreach(KeyValuePair<double, double> kvp in seeds)
+foreach (KeyValuePair<double, double> kvp in seeds)
 {
-    for(double i = kvp.Key; i < (kvp.Key + kvp.Value); i = i + 100000)
+    for (double i = kvp.Key; i < (kvp.Key + kvp.Value); i = i + 100000)
     {
-        if(!lowestValueAssigned)
+        if (!lowestValueAssigned)
         {
             lowestValue = SeedToLocation(i);
             seed = kvp;
             lowestSeed = i;
             lowestValueAssigned = true;
         }
-        else if(lowestValue > SeedToLocation(i))
+        else if (lowestValue > SeedToLocation(i))
         {
             lowestValue = SeedToLocation(i);
             seed = kvp;
@@ -33,59 +33,59 @@ foreach(KeyValuePair<double, double> kvp in seeds)
     }
 }
 Console.WriteLine($"lowestValue: {lowestValue}, seed: {seed.Key} - {seed.Key + seed.Value}, specific seed: {lowestSeed}");
-for(double i = 10000000; i >= 1; i = i/10)
+for (double i = 10000000; i >= 1; i = i / 10)
 {
 
-double filterSize = i;
-double RangeSearchFilter = Math.Floor(filterSize/10);
-while (RangeSearchFilter < 1)
-{
-Console.WriteLine("RangeSearchFilter start");
-    RangeSearchFilter++;
+    double filterSize = i;
+    double RangeSearchFilter = Math.Floor(filterSize / 10);
+    while (RangeSearchFilter < 1)
+    {
+        Console.WriteLine("RangeSearchFilter start");
+        RangeSearchFilter++;
+    }
+    Console.WriteLine("RangeSearchFilter stop");
+
+    KeyValuePair<double, double> range = GetLimits(seed, filterSize);
+
+    Console.WriteLine($"lowerlimit: {range.Key}, upperLimit: {range.Value}");
+
+    RangeSearch(range, RangeSearchFilter);
+    Console.WriteLine($"lowestValue: {lowestValue}, seed: {lowestSeed}");
 }
-Console.WriteLine("RangeSearchFilter stop");
 
-KeyValuePair<double, double> range = GetLimits(seed, filterSize); 
-
-Console.WriteLine($"lowerlimit: {range.Key}, upperLimit: {range.Value}");
-
-RangeSearch(range, RangeSearchFilter);
 Console.WriteLine($"lowestValue: {lowestValue}, seed: {lowestSeed}");
-}
 
-Console.WriteLine($"lowestValue: {lowestValue}, seed: {lowestSeed}");
+KeyValuePair<double, double> GetLimits(KeyValuePair<double, double> seed, double filterSize)
+{
 
-KeyValuePair<double, double> GetLimits (KeyValuePair<double, double> seed, double filterSize)
-{
-    
-double lowerLimit = lowestSeed - filterSize;
-double upperLimit = lowestSeed + filterSize;
-while(lowerLimit < seed.Key)
-{
-Console.WriteLine("lowerLimit start");
-    lowerLimit++;
-}
-Console.WriteLine("lowerLimit stop");
-while(upperLimit > (seed.Key + seed.Value))
-{
-Console.WriteLine("upperLimit start");
-    upperLimit --;
-}
-Console.WriteLine("upperLimit stop");
-    KeyValuePair<double, double> range = new KeyValuePair<double, double>(lowerLimit, upperLimit); 
-return range;
+    double lowerLimit = lowestSeed - filterSize;
+    double upperLimit = lowestSeed + filterSize;
+    while (lowerLimit < seed.Key)
+    {
+        Console.WriteLine("lowerLimit start");
+        lowerLimit++;
+    }
+    Console.WriteLine("lowerLimit stop");
+    while (upperLimit > (seed.Key + seed.Value))
+    {
+        Console.WriteLine("upperLimit start");
+        upperLimit--;
+    }
+    Console.WriteLine("upperLimit stop");
+    KeyValuePair<double, double> range = new KeyValuePair<double, double>(lowerLimit, upperLimit);
+    return range;
 }
 void RangeSearch(KeyValuePair<double, double> range, double filterSize)
 {
     for (double ii = range.Key; ii <= range.Value; ii = ii + filterSize)
     {
-        if(!lowestValueAssigned)
+        if (!lowestValueAssigned)
         {
             lowestSeed = ii;
             lowestValue = SeedToLocation(ii);
             lowestValueAssigned = true;
         }
-        else if(lowestValue > SeedToLocation(ii))
+        else if (lowestValue > SeedToLocation(ii))
         {
             lowestSeed = ii;
             lowestValue = SeedToLocation(ii);
@@ -104,7 +104,7 @@ double SeedToLocation(double seedValue)
         {
             getMap = false;
         }
-        else if(getMap)
+        else if (getMap)
         {
             string[] map = line.Split(" ");
             double location = 0;
@@ -112,9 +112,9 @@ double SeedToLocation(double seedValue)
             double startSource = double.Parse(map[1]);
             double mapLength = double.Parse(map[2]);
 
-            if(source >= startSource && source <= startSource + mapLength - 1)
+            if (source >= startSource && source <= startSource + mapLength - 1)
             {
-                location = mapStart + (source - startSource );
+                location = mapStart + (source - startSource);
                 source = location;
                 getMap = false;
             }
